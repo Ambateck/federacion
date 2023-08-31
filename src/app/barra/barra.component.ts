2b92d41d06a2bcce45e8b9f8db5fbf088f19b2df
ferarroba <fernandoarroba8@gmail.com>
2023-08-30 22:50:20 -0500

    Primer

--- src/app/barra/barra.component.ts
+++ src/app/barra/barra.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuxService } from '../services/aux.service';
 
 @Component({
   selector: 'app-barra',
@@ -7,7 +6,17 @@ import { AuxService } from '../services/aux.service';
   styleUrls: ['./barra.component.css']
 })
 export class BarraComponent {
-  constructor(private AuxService: AuxService) {
-    this.AuxService.loadScript();
+  items: Array<{ icon: any; text: string }> = [
+    { icon: 'fas fa-home', text: 'Home' },
+    { icon: 'fas fa-user', text: 'Profile' },
+    { icon: 'fas fa-comment', text: 'Message' },
+    { icon: 'fas fa-camera-retro', text: 'Photos' },
+    { icon: 'fas fa-cog', text: 'Settings' }
+  ];
+
+  activeIndex: number = 0;
+
+  setActive(index: number) {
+    this.activeIndex = index;
   }
 }
